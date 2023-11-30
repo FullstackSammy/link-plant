09b3b6b8173fecc8fcc92426576101bcd57ddaec
samzzii <nasstromsamuel@icloud.com>
2023-11-30 16:00:56 +0100

    added views for editing links and connected it to urls

--- link_plant/views.py
+++ link_plant/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 
 from .models import Profile, Link
 
@@ -15,3 +15,9 @@ class LinkCreateView(CreateView):
     success_url = reverse_lazy('link-list') # This here is where we send the user when they have successfully created a link. reverse_lazy takes in the url name as a parameter. 
     # This class creates a few things by default. the first thing is that it creates a template that takes the model name then _forms -> link_form.html
     # It will also create a form context variable that we will have access to inside the above html-file.
+    
+
+class LinkUpdateView(UpdateView):
+    model = Link
+    fields = ['text', 'url'] # specifies which DB fields become form fields to edit.
+    success_url = reverse_lazy('link-list') # This here is where we send the user when they have successfully created a link. reverse_lazy takes in the url name as a parameter.
